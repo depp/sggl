4e09e456fb054dfbacde103e80768e604381d000
Dietrich Epp <depp@zdome.net>
2015-01-19 12:57:22 -0800

    remove dependencies on SGLib

--- glgen/opengl_load.c
+++ glgen/opengl_load.c
@@ -1,9 +1,6 @@
 /* Copyright 2014-2015 Dietrich Epp.
    This file is part of SGGL.  SGGL is licensed under the terms of the
    2-clause BSD license.  For more information, see LICENSE.txt. */
-#include "private.h"
-#include "opengl_private.h"
-#include "sg/entry.h"
 #include "sggl/3_0.h"
 
 #include <assert.h>
@@ -15,22 +12,22 @@
 #endif
 
 /* Data defined in opengl_data.c, which is generated.  */
-extern const char SG_GL_ENTRYNAME[];
-#if defined SG_GL_HASEXTS
-extern const char SG_GL_EXTENSIONNAME[];
+extern const char SGGL_ENTRYNAME[];
+#if defined SGGL_HASEXTS
+extern const char SGGL_EXTENSIONNAME[];
 #endif
 
-int sg_glver;
-#if defined SG_GL_HASEXTS
-unsigned sg_glext[(SG_GL_EXTCOUNT + 31) / 32];
+int sggl_ver;
+#if defined SGGL_HASEXTS
+unsigned sggl_ext[(SGGL_EXTCOUNT + 31) / 32];
 #endif
-void *sg_glfunc[SG_GL_ENTRYCOUNT];
+void *sggl_func[SGGL_ENTRYCOUNT];
 
 #if defined __APPLE__
 #include <CoreFoundation/CoreFoundation.h>
 
-void
-sg_gl_getfuncs(const char *names, void **funcs, size_t count)
+int
+sggl_getfuncs(const char *names, void **funcs, size_t count)
 {
     CFBundleRef bundle;
     CFStringRef str;
@@ -80,10 +77,10 @@ sg_gl_getfuncs(const char *names, void **funcs, size_t count)
         CFRelease(cfnames[i]);
     free(cfnames);
     CFRelease(bundle);
-    return;
+    return 0;
 
 fail:
-    sg_sys_abort("Could not load OpenGL.");
+    return -1;
 }
 
 #elif defined _WIN32 || defined __linux__
@@ -95,8 +92,8 @@ extern void (*glXGetProcAddress(const GLubyte *procname))(void);
 #  define GETPROCADDRESS(name) glXGetProcAddress((const GLubyte *) name)
 # endif
 
-void
-sg_gl_getfuncs(const char *names, void **funcs, size_t count)
+int
+sggl_getfuncs(const char *names, void **funcs, size_t count)
 {
     char sname[128];
     size_t i, len;
@@ -111,28 +108,30 @@ sg_gl_getfuncs(const char *names, void **funcs, size_t count)
         p = p + len + 1;
         funcs[i] = GETPROCADDRESS(sname);
     }
+
+    return 0;
 }
 
 #else
 # error "Don't know how to load OpenGL functions on this platform."
 #endif
 
-#if defined SG_GL_HASEXTS
+#if defined SGGL_HASEXTS
 
 static void
-sg_gl_setext(const char *ext, size_t len)
+sggl_setext(const char *ext, size_t len)
 {
     const char *p;
-    int i, n = SG_GL_EXTCOUNT;
+    int i, n = SGGL_EXTCOUNT;
     size_t elen;
     if (memcmp(ext, "GL_", 3))
         return;
     ext += 3;
     len -= 3;
-    for (i = 0, p = SG_GL_EXTENSIONNAME; i < n; i++) {
+    for (i = 0, p = SGGL_EXTENSIONNAME; i < n; i++) {
         elen = strlen(p);
         if (elen == len && !memcmp(ext, p, len)) {
-            sg_glext[i / 32] |= 1u << (i & 31);
+            sggl_ext[i / 32] |= 1u << (i & 31);
             return;
         }
         p = p + elen + 1;
@@ -141,17 +140,19 @@ sg_gl_setext(const char *ext, size_t len)
 
 #endif
 
-void
-sg_gl_init(void)
+int
+sggl_init(void)
 {
     const char *version;
     int major, minor;
 
-    sg_gl_getfuncs(SG_GL_ENTRYNAME, sg_glfunc, SG_GL_ENTRYCOUNT);
+    if (sggl_getfuncs(SGGL_ENTRYNAME, sggl_func, SGGL_ENTRYCOUNT))
+        return -1;
 
     version = (const char *) glGetString(GL_VERSION);
     if (!version)
-        goto bad_version;
+        return -1;
+
     {
         const char *p;
         char *e;
@@ -159,30 +160,27 @@ sg_gl_init(void)
 
         x = strtoul(version, &e, 10);
         if (e == version || *e != '.' || x == 0)
-            goto bad_version;
+            return -1;
         major = (int) x;
         if (major >= 3) {
             major = minor = 0;
             glGetIntegerv(GL_MAJOR_VERSION, &major);
             glGetIntegerv(GL_MINOR_VERSION, &minor);
             if (major < 3)
-                goto bad_version;
+                return -1;
         } else {
             p = e + 1;
             x = strtoul(p, &e, 10);
             if (e == p || (*e != ' ' && *e != '.'))
-                goto bad_version;
+                return -1;
             minor = (int) x;
         }
-        if (major < 1 || major > 100) {
-        bad_version:
-            sg_sys_abortf("Invalid OpenGL version: %s.", version);
-            return;
-        }
+        if (major < 1 || major > 100)
+            return -1;
     }
-    sg_glver = major * 16 + (minor > 15 ? 15 : minor);
+    sggl_ver = major * 16 + (minor > 15 ? 15 : minor);
 
-#if defined SG_GL_HASEXTS
+#if defined SGGL_HASEXTS
     if (major < 3) {
         const char *p, *q;
         p = (const char *) glGetString(GL_EXTENSIONS);
@@ -190,10 +188,10 @@ sg_gl_init(void)
             while (1) {
                 q = strchr(p, ' ');
                 if (!q) {
-                    sg_gl_setext(p, strlen(p));
+                    sggl_setext(p, strlen(p));
                     break;
                 }
-                sg_gl_setext(p, q - p);
+                sggl_setext(p, q - p);
                 p = q + 1;
             }
         }
@@ -203,12 +201,10 @@ sg_gl_init(void)
         glGetIntegerv(GL_NUM_EXTENSIONS, &extcount);
         for (i = 0; i < extcount; i++) {
             ext = (const char *) glGetStringi(GL_EXTENSIONS, i);
-            sg_gl_setext(ext, strlen(ext));
+            sggl_setext(ext, strlen(ext));
         }
     }
 #endif
 
-    if (sg_cvar_developer.value) {
-        sg_gl_debug_init();
-    }
+    return 0;
 }
